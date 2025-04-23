7eab1b220c4b6578f73ca1787d9a9b6af36f3c0f
syyydong <3539546949@qq.com>
2025-04-23 17:23:47 +0800

    后端项目初始化

---
+++ src/main/java/com/yadong/sudada/model/dto/file/UploadFileRequest.java
@@ -0,0 +1,21 @@
+package com.yadong.sudada.model.dto.file;
+
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * 文件上传请求
+ *
+ * @author <a href="https://github.com/liyadong">程序员鱼皮</a>
+ * @from <a href="https://yadong.icu">编程导航知识星球</a>
+ */
+@Data
+public class UploadFileRequest implements Serializable {
+
+    /**
+     * 业务
+     */
+    private String biz;
+
+    private static final long serialVersionUID = 1L;
+}
